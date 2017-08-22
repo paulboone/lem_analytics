81e3c39f28f2dc25fac14c8195458548d1c616b6
Paul Boone <narcissus.pong@gmail.com>
2017-08-22 14:42:19 -0400

    linearized vs evolution

--- lem_numerical.py
+++ lem_numerical.py
@@ -6,16 +6,11 @@
 #
 #
 
+from collections import namedtuple
 
-m = 0.5
-n = 1.0
-k = 1
-d = 1
-
-
-import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.figure import figaspect
+import numpy as np
 
 def calc_drainage_area(z):
     results = np.zeros(len(z))
@@ -24,13 +19,13 @@ def calc_drainage_area(z):
         # look left
         j = i - 1
         while j >= 0 and z[j] > z[j + 1]:
-            drainage += 1
+            drainage +=  x_size
             j -= 1
 
         j = i + 1
         # look right
         while j < len(z) - 1 and z[j] > z[j - 1]:
-            drainage += 1
+            drainage += x_size
             j += 1
 
         results[i] = drainage
@@ -41,9 +36,9 @@ def calc_steepest_slope(z):
     for i,_ in enumerate(z):
         slopes = []
         if i > 0:
-            slopes.append(z[i - 1] - z[i])
+            slopes.append((z[i - 1] - z[i]) / x_size)
         if i < len(z) - 1:
-            slopes.append(z[i + 1] - z[i])
+            slopes.append(z[i + 1] - z[i] / x_size)
         results[i] = min(0, *slopes)
     return results
 
@@ -51,28 +46,64 @@ def calc_d2z_dx2(z):
     return np.gradient(np.gradient(z))
 
 
-def calc_dz_dt(z):
+def calc_dz_dt(z, c):
     a = calc_drainage_area(z)
     s = calc_steepest_slope(z)
     sp = calc_d2z_dx2(z)
-    return k*(a**m)*(s**n) + d * sp
+    return c.k*(a**c.m)*(s**c.n) + c.d * sp
 
 
+def plot(x, zs):
+    z_orig = zs[0]
+    num_plots = len(zs)
 
+    w, h = figaspect(0.5)
+    fig = plt.figure(figsize=(w,(h + 1) * num_plots))
 
-x = np.arange(0,100)
-z = np.arange(100,0, -1)
+    for i, z in enumerate(zs):
+        print(len(x), len(zs), len(z))
+        ax = fig.add_subplot(num_plots, 2, i*2 + 1)
+        ax.grid(linestyle='-', color='0.7', zorder=0)
+        # ax.set_ylim(0, 0.05)
+        ax.plot(x,z)
+        ax = fig.add_subplot(num_plots, 2, i*2 + 2)
+        ax.grid(linestyle='-', color='0.7', zorder=0)
+        ax.set_ylim(0, 0.05)
+        ax.plot(x,z_orig - z)
 
-dz_dt = calc_dz_dt(z)
-print(dz_dt)
-print(z + dz_dt)
+    fig.show()
 
-num_plots = 1
-w, h = figaspect(0.5)
-fig = plt.figure(figsize=(w,(h+1)*num_plots))
-ax = fig.add_subplot(num_plots, 1, plot_index)
-ax.plot(x,z)
+LemConsts = namedtuple('LemConsts', ['m', 'n', 'k', 'd'])
+lem_consts_t1 = LemConsts(0.5, 1.0, 5e-6, 3e-2)
+lem_consts_t1000 = LemConsts(0.5, 1.0, 5e-6 * 1000, 3e-2 * 1000)
 
+x_dist = 100
+x_size = 1
 
-# print(z1, len(z2))
-# print(z2, len(z2))
+timesteps = 1000
+plot_every = 100
+slope = -1
+
+# x = np.arange(0, x_dist / x_size)
+x = np.linspace(0.0, x_dist, x_dist / x_size + 1)
+z_orig = np.linspace(-slope * x_dist, 0.0, x_dist / x_size + 1)
+print(x, z_orig)
+
+zs = [z_orig]
+z = z_orig.copy()
+
+for plot_index in range(1, timesteps + 1):
+    z[-1] = 0.0
+    dz_dt = calc_dz_dt(z, lem_consts_t1)
+    z += dz_dt
+
+    if plot_index % plot_every == 0:
+        zs += [z.copy()]
+
+print("Total erosion (1000 timesteps)= %.2f" % (z - z_orig).sum())
+plot(x, zs)
+
+dz_dt = calc_dz_dt(z_orig, lem_consts_t1000)
+zs = [z_orig, z_orig + dz_dt]
+print("Total erosion (1 timesteps * 1000) = %.2f" % (dz_dt).sum())
+plot(x, zs)
