f7fffe9f6a91f3dfce164d82396bc549ce559d12
Paul Boone <narcissus.pong@gmail.com>
2017-08-23 11:06:46 -0400

    test for calc_steepest_slope

--- test_pylem.py
+++ test_pylem.py
@@ -1,5 +1,7 @@
 
-from pylem import calc_drainage_area
+import numpy as np
+
+from pylem import calc_drainage_area, calc_steepest_slope
 
 def test_calc_drainage_area_w_line_should_be_02020():
     assert list(calc_drainage_area([1,0,1,0,1], 1.0)) == [0.0,2.0,0.0,2.0,0.0]
@@ -15,5 +17,15 @@ def test_calc_drainage_area_deep_m_line_should_be_3210126210123():
 
 def test_calc_drainage_area_x_size_halved_should_halve_drainage_area():
     z = [10,8,4,2,1]
-    assert list(calc_drainage_area(z, 2)) == [0,2,4,6,8]
-    assert list(calc_drainage_area(z, 1)) == [0,1,2,3,4]
+    assert np.all(calc_drainage_area(z, 2) == calc_drainage_area(z, 1) * 2)
+
+
+def test_calc_steepest_slope_w_line_should_be_10101():
+    assert list(calc_steepest_slope([1,0,1,0,1], 1.0)) == [-1,0,-1,0,-1]
+
+def test_calc_steepest_slope_12401_line_should_be_02041():
+    assert list(calc_steepest_slope([1,2,4,0,1], 1.0)) == [0,-1,-4,0,-1]
+
+def test_calc_steepest_slope_x_size_halved_should_halve_slope():
+    z = [1,2,4,0,1]
+    assert np.all(calc_steepest_slope(z, 1.0) == calc_steepest_slope(z, 0.5) / 2)
